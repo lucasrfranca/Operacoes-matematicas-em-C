e331f803936f3c291b6602f9a52c3102fe81e40b
Lucas França <lucas.rfranca@gmail.com>
2022-08-05 16:29:21 -0300

    calculo de media pelo usuario

---
+++ media.c
@@ -0,0 +1,17 @@
+int main () {
+	
+	float n1, n2, media;
+	
+	printf ("Digite o primeiro numero: ");
+	scanf ("%f", &n1);
+	
+	printf ("Digite o segundo numero: ");
+	scanf ("%f", &n2);
+	
+	media = (n1+n2)/2;
+	
+	printf ("A media dos dois numeros = %f\n\n", media);
+	
+	system ("pause");
+
+}
