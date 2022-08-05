1c861c116c7f684477cddadc22c63ccc7986e301
Lucas França <lucas.rfranca@gmail.com>
2022-08-05 16:32:34 -0300

    calculo de divisao pelo usuario

---
+++ divisao.c
@@ -0,0 +1,17 @@
+int main () {
+	
+	float n1, n2, resultado;
+	
+	printf ("Digite o dividendo: ");
+	scanf ("%f", &n1);
+	
+	printf ("Digite o divisor: ");
+	scanf ("%f", &n2);
+	
+    resultado = n1/n2;
+	
+	printf ("Resultado = %f\n\n", resultado);
+	
+	system ("pause");
+
+}
