e7ae963cf1939878dc6c7768ae8704fc83b6e94b
Lucas França <lucas.rfranca@gmail.com>
2022-08-15 23:46:22 -0300

    cálculo de multiplicação de dois números inteiros

--- multiplicacao.c
+++ multiplicacao.c
@@ -1,6 +1,12 @@
-int main () {
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+main () {
+
+	setlocale(LC_ALL, "Portuguese");
 	
-	printf ("a multiplicacao de 10 e 2: ");
+	printf ("a multiplicação de 10 e 2 é: ");
 	printf ("%d", 10*2);
 	
     printf ("\n\n");
