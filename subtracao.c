8b7f82283ac4fe1200a00ea3b02c6c84fb11c6c9
Lucas França <lucas.rfranca@gmail.com>
2022-08-15 23:49:43 -0300

    cálculo de subtração de dois números inteiros

--- subtracao.c
+++ subtracao.c
@@ -1,6 +1,12 @@
-int main () {
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+main () {
+
+	setlocale(LC_ALL, "Portuguese");
 	
-	printf ("a subtracao de 8 e 6: ");
+	printf ("a subtração de 8 e 6 é: ");
 	printf ("%d", 8-6);
 	printf ("\n\n");
 	
